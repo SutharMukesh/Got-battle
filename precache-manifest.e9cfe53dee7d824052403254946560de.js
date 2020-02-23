682d5e06f8b6943a9c24aaa50bb51c726f237394
Mukesh Suthar <mukeshsuthar336@gmail.com>
2020-02-23 14:20:54 +0530

    Updates

---
+++ precache-manifest.e9cfe53dee7d824052403254946560de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0389998c9ce5e5a11f4e5bbab18b8d",
+    "url": "/Got-battle-react/index.html"
+  },
+  {
+    "revision": "b7a603a84519c3f5cd0a",
+    "url": "/Got-battle-react/static/css/main.0f8aa1f6.chunk.css"
+  },
+  {
+    "revision": "538c97a077fcbbdc06c8",
+    "url": "/Got-battle-react/static/js/2.8ae681ec.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Got-battle-react/static/js/2.8ae681ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a603a84519c3f5cd0a",
+    "url": "/Got-battle-react/static/js/main.5e9f4860.chunk.js"
+  },
+  {
+    "revision": "a969186a135ac9940a3a",
+    "url": "/Got-battle-react/static/js/runtime-main.e2055d30.js"
+  }
+]);
